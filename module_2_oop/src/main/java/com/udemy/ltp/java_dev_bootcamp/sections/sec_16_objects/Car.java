a15a347b0a2a3340293a734cecb1530494c4b725
Minh. Vu Tuan - CMC Global DU3.31 <vtminh1@cmcglobal.vn>
2022-11-15 12:46:49 +0700

    1st commit

---
+++ module_2_oop/src/main/java/com/udemy/ltp/java_dev_bootcamp/sections/sec_16_objects/Car.java
@@ -0,0 +1,10 @@
+package com.udemy.ltp.java_dev_bootcamp.sections.sec_16_objects;
+
+public class Car {
+	private String make;
+	private String color;
+	private double price;
+	private int year;
+
+
+}
---
+++ module_2_oop/src/main/java/com/udemy/ltp/java_dev_bootcamp/sections/sec_16_objects/Main.java
@@ -0,0 +1,7 @@
+package com.udemy.ltp.java_dev_bootcamp.sections.sec_16_objects;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
